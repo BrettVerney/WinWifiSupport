680fa22be4498445b6785a2f8fd757f293da45f4
Brett Verney <44787975+wifiwizardofoz@users.noreply.github.com>
2020-09-17 14:43:24 +1000

    Update WinWifiSupport.ps1

--- WinWifiSupport.ps1
+++ WinWifiSupport.ps1
@@ -1,3 +1,6 @@
+# Script by Brett Verney (@wifiwizardofoz)
+# Contributors - Felipe Binotto (@fbinotto)
+# Version: v0.2 | 17-09-2020
 $wifi = (netsh wlan show all | findstr /c:"Firmware Version" /c:"DOT11k" /c:"Transition" /c:"MU-MIMO" /c:"Spatial Streams" /c:"Management Frames" /c:"Driver                    :" /c:"Vendor  " /c:"Provider" /c:"Date" /c:"Version                   :" /c:"Radio types"  /c:"SAE Authentication" /c:"WPA3 SUITE-B Authentication" /c:"WPA3 SUITE-B FIPS Mode" /c:"OWE Authentication").trim()
 $objs = @()
 foreach ($_ in $wifi) {
