ace8eec2bcae5f9c9ec9a05735660b5861f14435
Brett Verney <44787975+wifiwizardofoz@users.noreply.github.com>
2020-09-17 14:31:54 +1000

    Create WinWifiSupport.ps1

---
+++ WinWifiSupport.ps1
@@ -0,0 +1,10 @@
+$wifi = (netsh wlan show all | findstr /c:"Firmware Version" /c:"DOT11k" /c:"Transition" /c:"MU-MIMO" /c:"Spatial Streams" /c:"Management Frames" /c:"Driver                    :" /c:"Vendor  " /c:"Provider" /c:"Date" /c:"Version                   :" /c:"Radio types"  /c:"SAE Authentication" /c:"WPA3 SUITE-B Authentication" /c:"WPA3 SUITE-B FIPS Mode" /c:"OWE Authentication").trim()
+$objs = @()
+foreach ($_ in $wifi) {
+    $newobj = New-Object pscustomobject
+    $newobj = $newobj | Select-Object Property, Value
+    $newobj.Property = $_.split(":")[0]
+    $newobj.Value = $_.split(":")[1].trim()
+    $objs += $newobj
+}
+$objs
