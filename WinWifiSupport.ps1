dbec9835ae24475f01a087090d349b5098992470
Brett Verney <44787975+wifiwizardofoz@users.noreply.github.com>
2021-05-11 15:02:52 +1000

    Remove duplicate MAC Randomization status

--- WinWifiSupport.ps1
+++ WinWifiSupport.ps1
@@ -1,7 +1,7 @@
 # Script by Brett Verney (@wifiwizardofoz)
 # Contributors - Felipe Binotto (@fbinotto)
 # Version: v1.0 | 30-09-2020
-$wifi = (netsh wlan show all | findstr /c:"Firmware Version" /c:"MAC Randomization" /c:"DOT11k" /c:"Transition" /c:"MU-MIMO" /c:"Spatial Streams" /c:"Management Frames" /c:"Driver                    :" /c:"Vendor  " /c:"Provider" /c:"Date" /c:"Version                   :" /c:"Radio types"  /c:"SAE Authentication" /c:"WPA3 SUITE-B Authentication" /c:"WPA3 SUITE-B FIPS Mode" /c:"OWE Authentication").trim()
+$wifi = (netsh wlan show all | findstr /c:"Firmware Version" /c:"MAC Randomization                           :" /c:"DOT11k" /c:"Transition" /c:"MU-MIMO" /c:"Spatial Streams" /c:"Management Frames" /c:"Driver                    :" /c:"Vendor  " /c:"Provider" /c:"Date" /c:"Version                   :" /c:"Radio types"  /c:"SAE Authentication" /c:"WPA3 SUITE-B Authentication" /c:"WPA3 SUITE-B FIPS Mode" /c:"OWE Authentication").trim()
 $objs = @()
 foreach ($_ in $wifi) {
     $newobj = New-Object pscustomobject
